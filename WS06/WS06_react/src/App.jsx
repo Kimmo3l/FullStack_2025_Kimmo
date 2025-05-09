a6b4b0498a32931dd97f9581c52e29b98aecf7ed
kimmo3l <kimmo.eloranta@student.laurea.fi>
2025-05-09 22:34:24 +0300

    väli save

--- WS06/WS06_react/src/App.jsx
+++ WS06/WS06_react/src/App.jsx
@@ -1,24 +1,41 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-import { nanoid } from "nanoid";
 
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
   
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  const [tasks, setTasks] = useState(props.tasks);
   const taskList = tasks?.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
